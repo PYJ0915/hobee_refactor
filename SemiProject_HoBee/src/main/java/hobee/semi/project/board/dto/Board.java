b7e7cb63ce21126fae15cbd3827ef882da6f9125
P1GEN6-SOLBI\bodun_56xh2zw <boduni20@gmail.com>
2025-12-23 16:01:05 +0900

    1223 공지게시판 작업

---
+++ SemiProject_HoBee/src/main/java/hobee/semi/project/board/dto/Board.java
@@ -0,0 +1,25 @@
+package hobee.semi.project.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Board {
+
+	private int boardNo;
+	private String boardTitle;
+	private String boardContent;
+	private String boardWriteDate;
+	private String boardUpdateDate;
+	private int boardViewCount;
+	private String boardDelFl;
+	private int memberNo;
+	private int boardCode;
+	
+
+}
