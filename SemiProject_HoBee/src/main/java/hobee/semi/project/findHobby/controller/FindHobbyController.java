c0c9f5aa4fd6a84f258d2175fff9efc33313bfd3
PYJ0915 <qkrdbwls3542@gmail.com>
2025-12-23 16:36:57 +0900

    다음 질문 얻어오기 구현 중

--- SemiProject_HoBee/src/main/java/hobee/semi/project/findHobby/controller/FindHobbyController.java
+++ SemiProject_HoBee/src/main/java/hobee/semi/project/findHobby/controller/FindHobbyController.java
@@ -3,6 +3,9 @@ package hobee.semi.project.findHobby.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("findHobby")
@@ -14,7 +17,8 @@ public class FindHobbyController {
 	}
 	
 	@GetMapping("start")
-	public String findHobbyStart() {
+	public String findHobbyStart(@RequestParam("inputName") String inputName, HttpSession session) {
+		session.setAttribute("inputName", inputName);
 		return "findHobby/question";
 	}
 	
---
+++ SemiProject_HoBee/src/main/resources/static/js/findHobby/question.js
@@ -0,0 +1,6 @@
+const question = document.querySelector(".question-title");
+
+const yBtn = document.querySelector("#y");
+const aBtn = document.querySelector("#a");
+const nBtn = document.querySelector("#n");
+
