49185eecb449d39c9f5709367e9eaa86f2c924ba
PYJ0915 <qkrdbwls3542@gmail.com>
2025-12-23 18:24:21 +0900

    질문 js 1차 완료

--- SemiProject_HoBee/src/main/resources/static/js/findHobby/question.js
+++ SemiProject_HoBee/src/main/resources/static/js/findHobby/question.js
@@ -1,6 +1,26 @@
 const question = document.querySelector(".question-title");
 
-const yBtn = document.querySelector("#y");
-const aBtn = document.querySelector("#a");
-const nBtn = document.querySelector("#n");
+const answerBtns = document.querySelectorAll(".answer");
 
+answerBtns.forEach((btn) => {
+
+  btn.addEventListener("click", nextQuestion())
+
+});
+
+function nextQuestion() {
+
+  fetch("/findHobby/selectQuestionList")
+  .then(resp => resp.json())
+  .then(questionList => {
+
+    let i = 1;
+
+    for(let inputQuestion of questionList) {
+      question.innerText = "Q" + i + "." + inputQuestion;
+      i++
+    }
+
+  });
+
+}
