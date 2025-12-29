ae57344df01ae6aefd630332aafab3ccf16f19b6
DESKTOP-K430LQ4\user1 <https://github.com/ycm93277211-ai>
2025-12-29 13:57:54 +0900

    0157

--- SemiProject_HoBee/src/main/resources/static/js/findHobby/findHobby.js
+++ SemiProject_HoBee/src/main/resources/static/js/findHobby/findHobby.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-const name = document.querySelector(".name-input").value;
-
-document.querySelector(".start-btn").addEventListener("click", () => {
-
-  location.href = "/findHobby/start";
-
-});
-
-=======
 const inputName = document.querySelector(".name-input");
 
 document.querySelector(".start-btn").addEventListener("click", () => {
@@ -15,4 +5,4 @@ document.querySelector(".start-btn").addEventListener("click", () => {
   location.href = "/findHobby/start?inputName=" + inputName.value;
 
 });
->>>>>>> cac63ee4428cb72110908c7fb2d02804905df2be
+
