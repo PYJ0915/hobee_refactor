ab3ed01acf948de625000a869880190396368bc0
PYJ0915 <qkrdbwls3542@gmail.com>
2026-01-02 11:49:41 +0900

    신고 기능 1차 구현 완료

---
+++ SemiProject_HoBee/src/main/java/hobee/semi/project/report/model/dto/Report.java
@@ -0,0 +1,22 @@
+package hobee.semi.project.report.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Report {
+
+	private int reportNo;
+	private String reportReason;
+	private String reportDetail;
+	private String reportDate;
+	private String reportStatus;
+	private int reporterMemberNo;
+	private int reportedMemberNo;
+	
+}
--- SemiProject_HoBee/src/main/resources/static/js/board/boardDetail.js
+++ SemiProject_HoBee/src/main/resources/static/js/board/boardDetail.js
@@ -3,6 +3,7 @@ const commentReportBtn = document.querySelector("#comment-report");
 const reportModal = document.querySelector("#reportModal");
 const reportWriter = document.querySelector("#reportWriter");
 const reportContent = document.querySelector("#reportContent");
+let reportedMemberNo = null;
 
 // 로그인한 회원의 회원번호
 const loginMemberNo = boardReportBtn.dataset.loginMemberNo;
@@ -27,6 +28,9 @@ function showReport(e) {
 
   const writer = e.currentTarget.dataset.writer;
 
+  // 신고당한 회원의 번호 얻어오기
+  reportedMemberNo = e.currentTarget.dataset.reportedMemberNo;
+
   reportWriter.innerText = writer;
 
   reportModal.classList.remove("popup-hidden");
@@ -37,9 +41,7 @@ document.querySelector(".submit-report-btn").addEventListener("click", () => {
 
   const checkedReason = document.querySelector('input[name="reason"]:checked');
   const reportDetail = document.querySelector(".detail-textarea").value;
-  const reportedMemberNickname = reportWriter.innerText;  
-  const reportedMemberNo = getMemberNo(reportedMemberNickname);
-  
+  const nickname = reportWriter.innerText;
 
   if(checkedReason == null) {
     alert("신고 사유를 선택해 주세요!")
@@ -50,7 +52,24 @@ document.querySelector(".submit-report-btn").addEventListener("click", () => {
     "reportReason" : checkedReason.value,
     "reportDetail" : reportDetail == "" ? null : reportDetail,
     "reporterMemberNo" : loginMemberNo,
-    "reportedMemberNo" : reportedMemberNo 
+    "reportedMemberNo" : reportedMemberNo
   };
 
+  console.log(obj);
+
+  fetch("/report/insertReport", {
+    method : "POST",
+    headers : {"Content-Type" : "application/json"},
+    body : JSON.stringify(obj)
+  })
+  .then(resp => resp.text())
+  .then(result => {
+    if(result > 0) {
+      alert(nickname + "님이 신고되었습니다.");
+      reportModal.classList.add("popup-hidden");
+    } else {
+      alert("신고에 실패했습니다. 다시 시도해주세요.");
+    }
+  });
 });
+
