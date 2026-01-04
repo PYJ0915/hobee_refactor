f2d5c74f449f0f7dc80c77bfe247a02c49562b95
PYJ0915 <qkrdbwls3542@gmail.com>
2026-01-04 13:13:59 +0900

    회원 제재 파트 구현 해야함

--- SemiProject_HoBee/src/main/resources/static/js/findHobby/findHobbyStart.js
+++ SemiProject_HoBee/src/main/resources/static/js/findHobby/findHobbyStart.js
@@ -1,8 +1,7 @@
 const inputName = document.querySelector(".name-input");
 
 document.querySelector(".start-btn").addEventListener("click", () => {
-
-  location.href = "/findHobby/start?inputName=" + inputName.value;
-
+  localStorage.setItem("inputName", inputName.value);
+  location.href = "/findHobby/start";
 });
 
