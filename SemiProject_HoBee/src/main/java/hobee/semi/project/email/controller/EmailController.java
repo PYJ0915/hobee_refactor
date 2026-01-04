870f8c9519fb5cdf56b9d9398308edd258dadff7
ycm93277211-ai <ycm93277211@gmail.com>
2026-01-04 19:03:23 +0900

    아이디 비번찾기 완

--- SemiProject_HoBee/src/main/java/hobee/semi/project/email/controller/EmailController.java
+++ SemiProject_HoBee/src/main/java/hobee/semi/project/email/controller/EmailController.java
@@ -13,29 +13,54 @@ import hobee.semi.project.email.model.service.EmailService;
 @Controller
 @RequestMapping("email")
 public class EmailController {
-	
+
 	@Autowired
 	private EmailService service;
 
-	// 이메일에 인증번호 보내기
+	// 회원가입 이메일에 인증번호 보내기
 	@ResponseBody
 	@PostMapping("signup")
 	public int signup(@RequestBody Email email) {
-		
+
 		String authEmail = email.getAuthEmail();
-		
-		return service.checkEmail ("signup",authEmail);
-		
+
+		return service.checkEmail("signup", authEmail);
+
 	}
-	
+
 	// 인증번호 확인
 	@ResponseBody
 	@PostMapping("checkAuthKey")
 	public int checkAuthKey(@RequestBody Email email) {
-		
+
 		String authKey = email.getAuthKey();
 		String authEmail = email.getAuthEmail();
-		
-		return service.checkAuthKey(authKey,authEmail);
+
+		return service.checkAuthKey(authKey, authEmail);
 	}
+
+	// 아이디 찾기 이메일에 인증번호 보내기 (재 사용)
+	@ResponseBody
+	@PostMapping("idSearch")
+	public int idserch(@RequestBody Email email) {
+
+		String authEmail = email.getAuthEmail();
+
+		return service.checkEmail("idSearch", authEmail);
+
+	}
+	
+	// 비밀번호 찾기 이메일에 인증번호 보내기 (재 사용)
+	@ResponseBody
+	@PostMapping("pwSearch")
+	public int pwserch(@RequestBody Email email) {
+		
+		String authEmail = email.getAuthEmail();
+		
+		return service.checkEmail("pwSearch", authEmail);
+		
+	}
+	
+	
+
 }
