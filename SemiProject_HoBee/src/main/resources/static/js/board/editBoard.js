278b8f986377a4f8943389ff6e4e48f44a15ea9d
P1GEN6-SOLBI\bodun_56xh2zw <boduni20@gmail.com>
2026-01-05 16:49:51 +0900

    0105

--- SemiProject_HoBee/src/main/resources/static/js/board/editBoard.js
+++ SemiProject_HoBee/src/main/resources/static/js/board/editBoard.js
@@ -2,6 +2,7 @@ $(document).ready(() => {
 
 
     $('#summernote').summernote({
+        placeholder: '내용을 입력해주세요',
         height: 400,
         lang: 'ko-KR',
         // 툴바 설정 추가
@@ -9,6 +10,7 @@ $(document).ready(() => {
             // [그룹명, [버튼 리스트]]
             ['style', ['style']], // 글머리 양식
             ['font', ['bold', 'underline', 'clear']], // 글자 굵게, 밑줄, 서식 지우기
+            ['fontsize', ['fontsize']], // 폰트 크기 버튼 추가
             ['fontname', ['fontname']], // 글꼴 설정
             ['color', ['color']], // 글자 색상
             ['para', ['ul', 'ol', 'paragraph']], // 리스트, 문단 정렬
@@ -16,11 +18,8 @@ $(document).ready(() => {
             ['insert', ['picture']], // 링크, 이미지, 비디오 -> 링크와 비디오는 구현이 쉽지 않아 삭제
             ['view', ['codeview', 'help']] // 전체화면, 소스코드 보기, 도움말 -> 전체화면 구현이 미흡하고 조잡해 삭제
         ],
-        callbacks: {
-            onImageUpload: (files) => { // 이미지 업로드 콜백
-                uploadImage(files[0], $('#summernote'));
-            }
-        }
+
+
     });
 });
 
