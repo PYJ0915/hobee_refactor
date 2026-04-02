ddfd1835d23c6738aeb29270e708edbf416b5967
PYJ0915 <qkrdbwls3542@gmail.com>
2026-04-02 09:51:15 +0900

    board 통합 작업 완료

--- SemiProject_HoBee/src/main/java/hobee/semi/project/board/controller/EditBoardController.java
+++ SemiProject_HoBee/src/main/java/hobee/semi/project/board/controller/EditBoardController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,27 +20,23 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import hobee.semi.project.board.model.dto.Board;
-import hobee.semi.project.board.model.service.EditBoardService;
-import hobee.semi.project.board.model.service.FreeBoardService;
-import hobee.semi.project.board.model.service.HobbyBoardService;
 import hobee.semi.project.board.model.service.BoardService;
+import hobee.semi.project.board.model.service.EditBoardService;
 import hobee.semi.project.member.model.dto.MemberDTO;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("editBoard")
-@RequiredArgsConstructor
 public class EditBoardController {
 
+	@Autowired
+	private EditBoardService service;
 
-	private final EditBoardService service;
-	private final FreeBoardService freeService;
-	private final HobbyBoardService hobbyService;
-	private final BoardService notiService;
-	
-	
-	/** 자유게시판 및 공지게시판 글작성 화면 조회
+	@Autowired
+	private BoardService boardService;
+
+	/**
+	 * 자유게시판 및 공지게시판 글작성 화면 조회
+	 * 
 	 * @param boardName
 	 * @return
 	 */
@@ -47,68 +44,76 @@ public class EditBoardController {
 	public String boardInsert(@PathVariable("boardName") String boardName) {
 		return "board/editBoard";
 	}
-	
-	/** 취미게시판 글작성 화면 조회
+
+	/**
+	 * 취미게시판 글작성 화면 조회
+	 * 
 	 * @param boardName
 	 * @param categoryCode
 	 * @return
 	 */
 	@GetMapping("/{boardName:[a-zA-Z]+}/{categoryCode:[0-9]+}/insert")
-    public String boardInsertHobby(
-            @PathVariable("boardName") String boardName,
-            @PathVariable("categoryCode") int categoryCode) {
-        return "board/editBoard";
-    }
-	
+	public String boardInsertHobby(@PathVariable("boardName") String boardName,
+			@PathVariable("categoryCode") int categoryCode) {
+		return "board/editBoard";
+	}
+
 	@PostMapping("/imageUpload")
-    @ResponseBody
-    public String imageUpload(@RequestParam("file") MultipartFile file) throws Exception {
-        return service.imageUpload(file);
-    }
-	
-	
-	/** 자유게시판 / 공지게시판 글작성 기능
+	@ResponseBody
+	public String imageUpload(@RequestParam("file") MultipartFile file) throws Exception {
+		return service.imageUpload(file);
+	}
+
+	/**
+	 * 자유게시판 / 공지게시판 글작성 기능
+	 * 
 	 * @return
 	 */
 	@PostMapping("/{boardName:[a-zA-Z]+}/insert")
-	public String boardInsert(@PathVariable("boardName") String boardName,
-			@ModelAttribute Board inputBoard,
-			@SessionAttribute("loginMember") MemberDTO loginMember,
-			RedirectAttributes ra
-			) throws IllegalStateException, IOException{
-		
+	public String boardInsert(@PathVariable("boardName") String boardName, @ModelAttribute Board inputBoard,
+			@SessionAttribute("loginMember") MemberDTO loginMember, RedirectAttributes ra)
+			throws IllegalStateException, IOException {
+
 		int boardCode = 0;
-	    switch(boardName) {
-	        case "notice" :  boardCode = 1; break; 
-	        case "hobby" :   boardCode = 2; break; 
-	        case "free" :    boardCode = 3; break;
-	    }
-		
+		switch (boardName) {
+		case "notice":
+			boardCode = 1;
+			break;
+		case "hobby":
+			boardCode = 2;
+			break;
+		case "free":
+			boardCode = 3;
+			break;
+		}
+
 		// 1. 로그인한 회원 번호를 세팅
-	    inputBoard.setMemberNo(loginMember.getMemberNo());
-	    // 2. 게시판 이름 세팅
-	    inputBoard.setBoardCode(boardCode);
+		inputBoard.setMemberNo(loginMember.getMemberNo());
+		// 2. 게시판 이름 세팅
+		inputBoard.setBoardCode(boardCode);
 
-	    // 3. 서비스 호출 (비즈니스 로직에서 이미지 DB 매칭 처리)
-	    int boardNo = service.boardInsert(inputBoard);
+		// 3. 서비스 호출 (비즈니스 로직에서 이미지 DB 매칭 처리)
+		int boardNo = service.boardInsert(inputBoard);
 
-	    String message = null;
-	    String path = null;
+		String message = null;
+		String path = null;
 
-	    if (boardNo > 0) {
-	        message = "게시글이 성공적으로 등록되었습니다.";
-	        path = "redirect:/" + boardName + "/" + boardNo; // 상세조회 페이지로
-	    } else {
-	        message = "게시글 등록에 실패했습니다. 다시 시도해 주세요.";
-	        path = "redirect:insert";
-	    }
+		if (boardNo > 0) {
+			message = "게시글이 성공적으로 등록되었습니다.";
+			path = "redirect:/" + boardName + "/" + boardNo; // 상세조회 페이지로
+		} else {
+			message = "게시글 등록에 실패했습니다. 다시 시도해 주세요.";
+			path = "redirect:insert";
+		}
 
-	    ra.addFlashAttribute("message", message);
-	    return path;
-		
+		ra.addFlashAttribute("message", message);
+		return path;
+
 	}
-	
-	/** 취미게시판 글작성 기능
+
+	/**
+	 * 취미게시판 글작성 기능
+	 * 
 	 * @param boardName
 	 * @param categoryCode
 	 * @param inputBoard
@@ -119,52 +124,56 @@ public class EditBoardController {
 	 * @throws IOException
 	 */
 	@PostMapping("/{boardName:[a-zA-Z]+}/{categoryCode:[0-9]+}/insert")
-	public String boardInsertHobby(
-	        @PathVariable("boardName") String boardName,
-	        @PathVariable("categoryCode") int categoryCode,
-	        @ModelAttribute Board inputBoard,
-	        @SessionAttribute("loginMember") MemberDTO loginMember,
-	        RedirectAttributes ra) throws IllegalStateException, IOException {
+	public String boardInsertHobby(@PathVariable("boardName") String boardName,
+			@PathVariable("categoryCode") int categoryCode, @ModelAttribute Board inputBoard,
+			@SessionAttribute("loginMember") MemberDTO loginMember, RedirectAttributes ra)
+			throws IllegalStateException, IOException {
 
 		int boardCode = 0;
-	    switch(boardName) {
-	        case "notice":  boardCode = 1; break; 
-	        case "hobby":   boardCode = 2; break; 
-	        case "free" :   boardCode = 3; break;
-	    }
-		
+		switch (boardName) {
+		case "notice":
+			boardCode = 1;
+			break;
+		case "hobby":
+			boardCode = 2;
+			break;
+		case "free":
+			boardCode = 3;
+			break;
+		}
+
 		// 1. 로그인한 회원 번호를 세팅
-	    inputBoard.setMemberNo(loginMember.getMemberNo());
-	    // 2. 게시판 이름 세팅
-	    inputBoard.setBoardCode(boardCode);
+		inputBoard.setMemberNo(loginMember.getMemberNo());
+		// 2. 게시판 이름 세팅
+		inputBoard.setBoardCode(boardCode);
 
-	    inputBoard.setCategoryCode(categoryCode);
+		inputBoard.setCategoryCode(categoryCode);
 
+		// 3. 서비스 호출 (비즈니스 로직에서 이미지 DB 매칭 처리)
+		int boardNo = service.boardInsert(inputBoard);
 
-	    // 3. 서비스 호출 (비즈니스 로직에서 이미지 DB 매칭 처리)
-	    int boardNo = service.boardInsert(inputBoard);
-	    
-	    String message = null;
-	    String path = null;
-	    
-	    
-	    if (boardNo > 0) {
-	        // 등록 성공 시
-	        message = "게시글이 성공적으로 등록되었습니다.";
-	        // 상세조회 페이지 경로 (예: /hobby/1/123)
-	        path = "redirect:/" + boardName + "/" + categoryCode + "/" + boardNo;
-	    } else {
-	        // 등록 실패 시
-	        message = "게시글 등록에 실패했습니다. 다시 시도해 주세요.";
-	        // 다시 글쓰기 화면으로 (상대 경로 insert)
-	        path = "redirect:insert";
-	    }
+		String message = null;
+		String path = null;
 
-	    ra.addFlashAttribute("message", message);
-	    return path;
+		if (boardNo > 0) {
+			// 등록 성공 시
+			message = "게시글이 성공적으로 등록되었습니다.";
+			// 상세조회 페이지 경로 (예: /hobby/1/123)
+			path = "redirect:/" + boardName + "/" + categoryCode + "/" + boardNo;
+		} else {
+			// 등록 실패 시
+			message = "게시글 등록에 실패했습니다. 다시 시도해 주세요.";
+			// 다시 글쓰기 화면으로 (상대 경로 insert)
+			path = "redirect:insert";
+		}
+
+		ra.addFlashAttribute("message", message);
+		return path;
 	}
-	
-	/** 공지게시판 / 자유게시판 삭제 메서드
+
+	/**
+	 * 공지게시판 / 자유게시판 삭제 메서드
+	 * 
 	 * @param boardName
 	 * @param boardNo
 	 * @param cp
@@ -172,47 +181,40 @@ public class EditBoardController {
 	 * @param ra
 	 * @return
 	 */
-	@RequestMapping(value="/{boardName:[a-zA-Z]+}/{boardNo:[0-9]+}/delete",
-			method= {RequestMethod.POST})
-public String BoardDelete(@PathVariable("boardName") String boardName,
-						@PathVariable("boardNo") int boardNo,
-						@RequestParam(value="cp", required = false, defaultValue = "1") int cp,
-						@SessionAttribute("loginMember") MemberDTO loginMember,
-						RedirectAttributes ra ) { 
-		
+	@RequestMapping(value = "/{boardName:[a-zA-Z]+}/{boardNo:[0-9]+}/delete", method = { RequestMethod.POST })
+	public String BoardDelete(@PathVariable("boardName") String boardName, @PathVariable("boardNo") int boardNo,
+			@RequestParam(value = "cp", required = false, defaultValue = "1") int cp,
+			@SessionAttribute("loginMember") MemberDTO loginMember, RedirectAttributes ra) {
+
 		Map<String, Object> map = new HashMap<>();
 		map.put("boardName", boardName);
 		map.put("memberNo", loginMember.getMemberNo());
 		map.put("boardNo", boardNo);
 		map.put("authorLevel", loginMember.getAuthorLevel());
 		map.put("categoryCode", 0);
-		
+
 		int result = service.boardDelete(map);
-		
+
 		String path = null;
 		String message = null;
-		
-		if(result > 0) {
+
+		if (result > 0) {
 			path = String.format("/%s?cp=%d", boardName, cp);
-							   
+
 			message = "글 삭제가 완료되었습니다";
-			
+
 		} else {
 			path = String.format("/%s/%d/?cp=%d", boardName, boardNo, cp);
-			
+
 			message = "삭제 실패됨 ....";
 		}
-		
 		ra.addFlashAttribute("message", message);
-		
-		
 		return "redirect:" + path;
-		
-	
 	}
-	
-	
-	/** 취미게시판 삭제 메서드
+
+	/**
+	 * 취미게시판 삭제 메서드
+	 * 
 	 * @param boardName
 	 * @param categoryCode
 	 * @param boardNo
@@ -221,46 +223,45 @@ public String BoardDelete(@PathVariable("boardName") String boardName,
 	 * @param ra
 	 * @return
 	 */
-	@RequestMapping(value="/{boardName:[a-zA-Z]+}/{categoryCode:[0-9]+}/{boardNo:[0-9]+}/delete",
-			method= {RequestMethod.POST})
-public String hobbyBoardDelete(@PathVariable("boardName") String boardName,
-						@PathVariable("categoryCode") int categoryCode,
-						@PathVariable("boardNo") int boardNo,
-						@RequestParam(value="cp", required = false, defaultValue = "1") int cp,
-						@SessionAttribute("loginMember") MemberDTO loginMember,
-						RedirectAttributes ra ) { 
-		
+	@RequestMapping(value = "/{boardName:[a-zA-Z]+}/{categoryCode:[0-9]+}/{boardNo:[0-9]+}/delete", method = {
+			RequestMethod.POST })
+	public String hobbyBoardDelete(@PathVariable("boardName") String boardName,
+			@PathVariable("categoryCode") int categoryCode, @PathVariable("boardNo") int boardNo,
+			@RequestParam(value = "cp", required = false, defaultValue = "1") int cp,
+			@SessionAttribute("loginMember") MemberDTO loginMember, RedirectAttributes ra) {
+
 		Map<String, Object> map = new HashMap<>();
 		map.put("boardName", boardName);
 		map.put("categoryCode", categoryCode);
 		map.put("memberNo", loginMember.getMemberNo());
 		map.put("boardNo", boardNo);
 		map.put("authorLevel", loginMember.getAuthorLevel());
-		
+
 		int result = service.boardDelete(map);
-		
+
 		String path = null;
 		String message = null;
-		
-		if(result > 0) {
+
+		if (result > 0) {
 			path = String.format("/hobby/%d?cp=%d", categoryCode, cp);
-							   // /hobby/1/?cp=1
+			// /hobby/1/?cp=1
 			message = "글 삭제가 완료되었습니다";
-			
+
 		} else {
 			path = String.format("/hobby/%d/%d?cp=%d", categoryCode, boardNo, cp);
-							   // /hobby/1/24?cp=1
+			// /hobby/1/24?cp=1
 			message = "삭제 실패됨 ....";
 		}
-		
+
 		ra.addFlashAttribute("message", message);
-		
-		
+
 		return "redirect:" + path;
-		
+
 	}
-	
-	/** 게시글 수정 화면 이동
+
+	/**
+	 * 게시글 수정 화면 이동
+	 * 
 	 * @param boardName
 	 * @param boardNo
 	 * @param categoryCode
@@ -270,40 +271,30 @@ public String hobbyBoardDelete(@PathVariable("boardName") String boardName,
 	 * @return
 	 * @throws Exception
 	 */
-	@GetMapping(value = {
-		    "/{boardName:[a-zA-Z]+}/{boardNo:[0-9]+}/update",
-		    "/{boardName:[a-zA-Z]+}/{categoryCode:[0-9]+}/{boardNo:[0-9]+}/update"
-		})
-		public String boardUpdate(
-		        @PathVariable("boardName") String boardName,
-		        @PathVariable("boardNo") int boardNo,
-		        @PathVariable(value="categoryCode", required=false) Integer categoryCode, // Integer는 null을 포함하기 때문에 사용해야함
-		        @SessionAttribute("loginMember") MemberDTO loginMember,
-		        Model model,
-		        RedirectAttributes ra) throws Exception {
-		
+	@GetMapping(value = { "/{boardName:[a-zA-Z]+}/{boardNo:[0-9]+}/update",
+			"/{boardName:[a-zA-Z]+}/{categoryCode:[0-9]+}/{boardNo:[0-9]+}/update" })
+	public String boardUpdate(@PathVariable("boardName") String boardName, @PathVariable("boardNo") int boardNo,
+			@PathVariable(value = "categoryCode", required = false) Integer categoryCode, // Integer는 null을 포함하기 때문에
+																							// 사용해야함
+			@SessionAttribute("loginMember") MemberDTO loginMember, Model model, RedirectAttributes ra)
+			throws Exception {
+
 		Map<String, Object> map = new HashMap<>();
 		map.put("boardNo", boardNo);
-		
-		
+
 		Board board = null;
 
-	    switch(boardName) {
-	        case "notice" : board = notiService.selectBoardDetail(map); break;
-	        case "free"   : board = freeService.selectBoardDetail(map); break;
-	        case "hobby"  : board = hobbyService.selectBoardDetail(map); break;
-	    }
+		board = boardService.selectBoardDetail(map);
 
-	    
-	    model.addAttribute("board", board);
-	    model.addAttribute("boardName", boardName); 
-	    
-	    return "board/editBoard";
+		model.addAttribute("board", board);
+		model.addAttribute("boardName", boardName);
+
+		return "board/editBoard";
 	}
-	
-	
-	
-	/** 게시글 수정 적용
+
+	/**
+	 * 게시글 수정 적용
+	 * 
 	 * @param boardName
 	 * @param boardNo
 	 * @param categoryCode
@@ -313,62 +304,46 @@ public String hobbyBoardDelete(@PathVariable("boardName") String boardName,
 	 * @param ra
 	 * @return
 	 */
-	@PostMapping(value = {
-		    "/{boardName:[a-zA-Z]+}/{boardNo:[0-9]+}/update",
-		    "/{boardName:[a-zA-Z]+}/{categoryCode:[0-9]+}/{boardNo:[0-9]+}/update"
-		})
-	public String boardUpdate(
-			@PathVariable("boardName") String boardName,
-			@PathVariable("boardNo") int boardNo,
-			@PathVariable(value = "categoryCode", required = false) Integer categoryCode,
-			Board inputBoard,
+	@PostMapping(value = { "/{boardName:[a-zA-Z]+}/{boardNo:[0-9]+}/update",
+			"/{boardName:[a-zA-Z]+}/{categoryCode:[0-9]+}/{boardNo:[0-9]+}/update" })
+	public String boardUpdate(@PathVariable("boardName") String boardName, @PathVariable("boardNo") int boardNo,
+			@PathVariable(value = "categoryCode", required = false) Integer categoryCode, Board inputBoard,
 			@RequestParam(value = "cp", required = false, defaultValue = "1") int cp,
-			@SessionAttribute("loginMember") MemberDTO loginMember,
-			RedirectAttributes ra
-			) {
-		
+			@SessionAttribute("loginMember") MemberDTO loginMember, RedirectAttributes ra) {
+
 		inputBoard.setBoardNo(boardNo);
 		inputBoard.setMemberNo(loginMember.getMemberNo());
 		inputBoard.setAuthorLevel(loginMember.getAuthorLevel());
-		if(categoryCode != null) {
+		if (categoryCode != null) {
 			inputBoard.setCategoryCode(categoryCode);
 		}
-		
+
 		int result = service.boardUpdate(inputBoard);
-		
+
 		String message = null;
 		String path = null;
-		
-		if(result > 0) {
+
+		if (result > 0) {
 			message = "게시글이 수정 되었습니다";
-			
-				    switch(boardName) {
-			        case "notice" : path = String.format("/notice/%d?cp=%d", boardNo, cp); break;
-			        case "free"   : path = String.format("/free/%d?cp=%d", boardNo, cp); break;
-			        case "hobby"  : path = String.format("/hobby/%d/%d?cp=%d", categoryCode, boardNo, cp); break;
-			    }
-				    
-				
-			
+
+			switch (boardName) {
+			case "notice":
+				path = String.format("/notice/%d?cp=%d", boardNo, cp);
+				break;
+			case "free":
+				path = String.format("/free/%d?cp=%d", boardNo, cp);
+				break;
+			case "hobby":
+				path = String.format("/hobby/%d/%d?cp=%d", categoryCode, boardNo, cp);
+				break;
+			}
 		} else {
 			message = "수정 실패";
 			path = "update";
-			
+
 		}
-		
 		ra.addFlashAttribute("message", message);
-		
 		return "redirect:" + path;
-		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
